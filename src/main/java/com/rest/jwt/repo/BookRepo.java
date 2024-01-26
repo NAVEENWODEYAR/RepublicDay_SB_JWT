e2e71080a74e55124675967be129ef761fdb0e9d
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-27 02:18:49 +0530

    java.lang.IllegalArgumentException: Invalid character found in method name [0x160x030x010x000xf70x010x000x000xf30x030x030x040xa10x910xe80x90Y0xefDHB0xcf0xbde0x1d0xb70xdc0xcd0xa00xba0x140xce,i0xa87>0xc7R0xd30xa70x8e0x05 ]. HTTP method names must be tokens

--- src/main/java/com/rest/jwt/repo/BookRepo.java
+++ src/main/java/com/rest/jwt/repo/BookRepo.java
@@ -1,9 +1,9 @@
 package com.rest.jwt.repo;
 
 import com.rest.jwt.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface BookRepo extends JpaRepository<BookEntity,Long> {
+public interface BookRepo extends CrudRepository<BookEntity,Long> {
 }
